e68ef73799aa1126fcc35508ba8b0e9a7412dcfc
Bruno <bkozina@volum3.com>
2021-12-02 23:00:32 +0100

    Add react router and packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
 import './App.css';
+import { render } from "react-dom";
+import {
+    BrowserRouter,
+    Routes,
+    Route
+} from "react-router-dom";
+import PokemonList from './Views/PokemonList';
+import PokemonDetails from './Views/PokemonDetails';
 
-function App() {
-  return (
-    <div className="App">
-      asd
-    </div>
-  );
+const App = () => {
+    return (
+        <Routes>
+            <Route path="/" element={<PokemonList />}>
+                <Route path="pokemon/:pokemonId" element={<PokemonDetails />} />
+            </Route>
+        </Routes>
+    )
 }
 
 export default App;
