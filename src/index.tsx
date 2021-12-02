66e620d4456f9a96d4fc8292eb7320d6ae253a6c
Bruno <bkozina@volum3.com>
2021-12-02 23:16:35 +0100

    fix import bug

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './Views/PokemonList';
+import App from './App';
 import { PokemonProvider } from './PokemonContext';
 import { BrowserRouter } from 'react-router-dom';
 
