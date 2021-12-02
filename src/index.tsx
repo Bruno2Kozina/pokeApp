2e1a3fb61c29c45c39e0f951c80f4560660110fb
Bruno <bkozina@volum3.com>
2021-12-02 23:03:58 +0100

    fix index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './Views/PokemonList';
+import { PokemonProvider } from './PokemonContext';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <PokemonProvider>
+        <App />
+      </PokemonProvider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
