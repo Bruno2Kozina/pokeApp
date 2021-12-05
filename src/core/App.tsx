44d1c80d19a6a10bd19691bb8b43daeaa675900d
Bruno <bkozina@volum3.com>
2021-12-05 14:59:47 +0100

    fix pagination and add background + update style

--- src/core/App.tsx
+++ src/core/App.tsx
@@ -1,6 +1,5 @@
-
 import PokemonList from '../Views/PokemonList';
-import React, {useContext, useEffect} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import axios from 'axios';
 import { PokemonContext } from '../core/PokemonContext';
 import { MainWrapper, Pagination} from '../core/Styles'
@@ -18,30 +17,49 @@ interface PokeApiResponse {
     results: PostType[]
 }
 
+interface PokeTypeApiResponse {
+    count: number
+    previous: string
+    next: string
+    results: PostType[]
+}
+
 const App = () => {
 
     const pokemonContext = useContext(PokemonContext);
-    const { pokemons, addPokemons, searchValue, changeSearchValue, pageNumber, changePageNumber, pokemonCount, changePokemonCount, itemsPerPage, changeItemsPerPage, isLoading, changeIsLoading } = pokemonContext
+    const { pokemons, addPokemons, pokeTypes, addPokeTypes, searchValue, changeSearchValue, pageNumber, changePageNumber, pokemonCount, changePokemonCount, itemsPerPage, changeItemsPerPage, changeIsLoading } = pokemonContext
 
-  useEffect(() => {
-    const fetchPokemons = async () => {
-        const response = await axios.get<PokeApiResponse>(`https://pokeapi.co/api/v2/pokemon?limit=${itemsPerPage}&offset=${pageNumber * itemsPerPage}`);
-        addPokemons(response.data.results)
-        changePokemonCount(response.data.count)
-        changeIsLoading(false)
-        }
+    const [nextResponse, setNextResponse] = useState('')
+    const [hasMore, setHasMore] = useState(true)
+    
+    useEffect(() => {
+        const fetchPokemons = async () => {
+            const response = await axios.get<PokeApiResponse>(`https://pokeapi.co/api/v2/pokemon?limit=${itemsPerPage}&offset=${pageNumber * itemsPerPage}`);
+            addPokemons(response.data.results)
+            changePokemonCount(response.data.count)
+            changeIsLoading(false)
+            setNextResponse(response.data.next)
+            }
         fetchPokemons();
     }, [pageNumber, itemsPerPage])
 
+    useEffect(() => {
+        const fetchPokemonTypes = async () => {
+            const response = await axios.get<PokeTypeApiResponse>('https://pokeapi.co/api/v2/type/');
+            addPokeTypes(response.data.results)
+        }
+        fetchPokemonTypes()
+    }, [])
+
     const changePage = ({ selected }: any) => {
         changePageNumber(selected);
-  };
-
+    };
 
     return (
         <MainWrapper>
             <div>
                 <input 
+                onFocus={() => changeItemsPerPage(pokemonCount)} // only solution to search from list of all pokemons
                 className='findPokeInput'
                 type="search"
                 name="search"
@@ -50,23 +68,31 @@ const App = () => {
                 onChange={e => changeSearchValue(e.target.value)} />
                 
             </div>
-            <PokemonList/>
+            <select>
+                 {pokeTypes.map(type => <option>{type.name}</option>)}    
+            </select>
+            <PokemonList key={Math.random() * 100}/>
             <Pagination>
-                <div className='paginationButtonsWrapper'>
-                <ReactPaginate 
-                    previousLabel={"Previous"}
-                    nextLabel={"Next"}
-                    onPageChange={changePage}
-                    pageCount={pokemonCount / itemsPerPage}
-                    containerClassName={"paginationBttns"}
-                    previousLinkClassName={"previousBttn"}
-                    nextLinkClassName={"nextBttn"}
-                    disabledClassName={"paginationDisabled"}
-                    activeClassName={"paginationActive"} />
-                </div>
-                <input onBlur={(e) => changeItemsPerPage(e.target.value)} type='number' name='number'/>
-                <label>items per page</label>
+                {(itemsPerPage < pokemonCount) && <div>
+                    <ReactPaginate 
+                        previousLabel={"Previous"}
+                        nextLabel={"Next"}
+                        onPageChange={changePage}
+                        pageCount={pokemonCount / itemsPerPage}
+                        containerClassName={"paginationBttns"}
+                        previousLinkClassName={"previousBttn"}
+                        nextLinkClassName={"nextBttn"}
+                        disabledClassName={"paginationDisabled"}
+                        activeClassName={"paginationActive"} />
+                </div>}
             </Pagination>
+            <select className='itemsPerPage' onChange={(e) => changeItemsPerPage(e.target.value)}>
+                <option value='20'>20</option>
+                <option value='40'>40</option>
+                <option value='100'>100</option>
+                <option value='200'>200</option>
+                <option value={pokemonCount}>All</option>
+            </select>
     </MainWrapper>
     )
 }
--- src/core/Styles.tsx
+++ src/core/Styles.tsx
@@ -1,22 +1,22 @@
 import styled from 'styled-components'
-
-//GLOBAL
-export const GlobalContainer = styled.div`
- 
-`
-
-
-//Pokemon List
-
-
+import backgroundLightImg from '../Assets/Images/backgroundLightImg.jpeg'
 
 export const MainWrapper = styled("div")<{}>`
-    background-color: orange;
+    background-image: url(${backgroundLightImg});
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
     height: 100vh;
+    position: relative;
+
+    .itemsPerPage {
+        position: absolute;
+        bottom: 30px;
+        right: 300px;
+        height: 30px;
+        border: none;
+    }
 
     .findPokeInput {
         margin-top: 20px;
@@ -28,13 +28,14 @@ export const ListContainerBox = styled("div")<{}>`
     justify-content: center;
     height: 80vh;
     width: 100%;
-    padding: 10px 0;
 `
 
 export const ListContainer = styled("div")<{}>`
     display: flex;
     flex-flow: row wrap;
-    width: 70%;    
+    width: 70%;
+    margin: 10px 0;
+    padding: 4px 20px;
     max-height: 100%;
     justify-content: space-between;
     overflow-y: auto;
@@ -44,36 +45,18 @@ export const Pagination = styled.div`
     display: flex;
     position: relative;
     height: 10vh;
-    width: 60%;
-    margin: auto;
-
-    .paginationButtonsWrapper {
-        align-items: center;
-        justify-content: center;
-    }
-
-    label {
-        font-size: 10px;
-        color: white;
-    }
-
-    input::-webkit-outer-spin-button,
-    input::-webkit-inner-spin-button {
-        -webkit-appearance: none;
-    }
 
     .paginationBttns {
         width: 80%;
         height: 40px;
         list-style: none;
         display: flex;
-        justify-content: center;
     }
   
     .paginationBttns a {
         padding: 6px;
         margin: 6px;
-        color: white;
+        color: blue;
         cursor: pointer;
     }
     
@@ -82,14 +65,10 @@ export const Pagination = styled.div`
     }
     
     .paginationActive a {
-        color: white;
+        color: blue;
         background-color: red;
+        border-radius: 50%;
     }
-`
-
-export const InputComponent = styled.div`
-
-    
 `
 
 export const CardWrapper = styled.div`
@@ -97,7 +76,8 @@ export const CardWrapper = styled.div`
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    background-color: white;
+    background-color: rgb(178,34,34, 0.7);
+    opacity: 0.9;
     width: 160px;
     height: 160px;
     margin: 10px;
@@ -108,10 +88,9 @@ export const CardWrapper = styled.div`
     &:hover {
         transform: scale(1.07);
         border-radius: 20px;
+        opacity: 1;
         box-shadow: 0px 8px 21px 0px #000000;
-      
-    
-  }
+    }
 
 `
 
@@ -121,4 +100,3 @@ export const CardName = styled.p`
     font-size: 20px;
     font-weight: 600;
 `
-
