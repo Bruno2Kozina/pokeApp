af5c8aade730dd8bd190e6a46fa13075cfbb8cf6
Bruno <bkozina@volum3.com>
2021-12-05 15:01:26 +0100

    Add image placeholder

--- src/Components/PokemonCard.tsx
+++ src/Components/PokemonCard.tsx
@@ -1,14 +1,13 @@
-import React, {FC, useContext, useEffect, useState} from 'react';
-import axios from 'axios';
-import { Link, NavLink } from 'react-router-dom';
+import React, {FC} from 'react';
+import { Link } from 'react-router-dom';
 import {CardWrapper, CardName} from '../core/Styles'
-import PokeBall from '../Assets/Images/Pokeball-alt.jpeg'
-
+import {imageOnErrorHandler} from '../core/Utils'
 
 type IProps = {
     url: string
     name: string
 }
+
 
 const Pokemon: FC<IProps> = ({ name, url }) => {
 
@@ -16,21 +15,17 @@ const Pokemon: FC<IProps> = ({ name, url }) => {
   const pokeIndex = pokeUrl[pokeUrl.length - 2]
   const pokeImage = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokeIndex}.png`
 
-  const imageOnErrorHandler = (e: React.SyntheticEvent<HTMLImageElement, Event>) => {
-    e.currentTarget.src = PokeBall;
-  };
-
   return (
+    <div> {
      <Link
         style={{ textDecoration: 'none', color: 'black' }}
         to={`pokemon/${pokeIndex}`} // Link to PokemonDetails.tsx
-        state={{name}}
         >
       <CardWrapper>
         <img style={{ width: '6em', height: '6em', paddingTop: '20px' }} src={pokeImage} onError={imageOnErrorHandler}/> 
         <CardName>{name}</CardName>
       </CardWrapper>
-      </Link>
+      </Link>}</div>
   );
 }
 
