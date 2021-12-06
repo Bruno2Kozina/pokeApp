2650383d44ad5c7aefcb23d99246ccdc3d414679
Bruno <bkozina@volum3.com>
2021-12-06 19:07:16 +0100

    Add filter by type

--- src/Views/App.tsx
+++ src/Views/App.tsx
@@ -1,13 +1,18 @@
 import PokemonList from '../Components/PokemonList';
-import React, {useContext, useEffect, useState} from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import axios from 'axios';
 import { PokemonContext } from '../core/PokemonContext';
-import { MainWrapper, Pagination} from '../core/Styles'
+import { MainWrapper, Pagination } from '../core/Styles'
 import ReactPaginate from 'react-paginate';
-import {isMobile} from 'react-device-detect'
+import { isMobile } from 'react-device-detect'
+import InfiniteScroll from "react-infinite-scroll-component";
+import Loader from '../Components/Loader';
+import { SelectHTMLAttributes } from 'react';
+import { DetailedHTMLProps } from 'react';
 
-export interface PostType {
-	name: string
+
+export interface PokemonType {
+    name: string
     url: string
 }
 
@@ -15,34 +20,53 @@ interface PokeApiResponse {
     count: number
     previous: string
     next: string
-    results: PostType[]
+    results: PokemonType[]
+}
+
+interface PokemonsByTypeApiResponse {
+    pokemon: {pokemon: PokemonType}[]
 }
 
 interface PokeTypeApiResponse {
     count: number
     previous: string
     next: string
-    results: PostType[]
+    results: PokemonType[]
 }
 
 const App = () => {
 
     const pokemonContext = useContext(PokemonContext);
-    const { pokemons, addPokemons, pokeTypes, addPokeTypes, searchValue, changeSearchValue, pageNumber, changePageNumber, pokemonCount, changePokemonCount, itemsPerPage, changeItemsPerPage, changeIsLoading } = pokemonContext
+    const { pokemons, addPokemons, pokeTypes, addPokeTypes, searchValue, changeSearchValue, pageNumber, changePageNumber, pokemonCount, changePokemonCount, itemsPerPage, changeItemsPerPage, changeIsLoading, isLoading } = pokemonContext
 
     const [nextResponse, setNextResponse] = useState('')
     const [hasMore, setHasMore] = useState(true)
-    
+    const [typeFilter, setTypeFilter] = useState('none')
+
+
+    const fetchPokemons = async () => {
+        const response = await axios.get<PokeApiResponse>(`https://pokeapi.co/api/v2/pokemon?limit=${itemsPerPage}&offset=${pageNumber * itemsPerPage}`);
+        addPokemons(response.data.results)
+        changePokemonCount(response.data.count)
+        changeIsLoading(false)
+    }
+
+    const fetchPokemonsByType = async () => {
+        const response = await axios.get<PokemonsByTypeApiResponse>(`https://pokeapi.co/api/v2/type/${typeFilter}`);
+        addPokemons(response.data.pokemon.map(poke => poke.pokemon))
+        changeIsLoading(false)
+        changeItemsPerPage(pokemonCount);
+    }
+
     useEffect(() => {
-        const fetchPokemons = async () => {
-            const response = await axios.get<PokeApiResponse>(`https://pokeapi.co/api/v2/pokemon?limit=${itemsPerPage}&offset=${pageNumber * itemsPerPage}`);
-            addPokemons(response.data.results)
-            changePokemonCount(response.data.count)
-            changeIsLoading(false)
-            setNextResponse(response.data.next)
-            }
-        fetchPokemons();
-    }, [pageNumber, itemsPerPage])
+        if (typeFilter == 'none') {
+            fetchPokemons();
+        } else {
+            fetchPokemonsByType();
+        }
+    }, [pageNumber, itemsPerPage, typeFilter])
+
+    console.log(typeFilter)
 
     useEffect(() => {
         const fetchPokemonTypes = async () => {
@@ -56,27 +80,39 @@ const App = () => {
         changePageNumber(selected);
     };
 
+    const onTypeFilterChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        setTypeFilter(e.target.value)
+    }
+
     return (
         <MainWrapper>
-            <div>
+            <div className='headerWrapper'>
                 <input
-                onFocus={() => changeItemsPerPage(pokemonCount)} // only solution to search from list of all pokemons
-                className='findPokeInput'
-                type="search"
-                name="search"
-                placeholder="Find your pokemon"
-                value={searchValue}
-                onChange={e => changeSearchValue(e.target.value)} />
-                <select>
-                    <option selected>none</option>
-                    {pokeTypes.map(type => <option>{type.name}</option>)}    
+                    onFocus={() => changeItemsPerPage(pokemonCount)} // only solution to search from list of all pokemons
+                    className='findPokeInput'
+                    type="search"
+                    name="search"
+                    placeholder="Find your pokemon"
+                    value={searchValue}
+                    onChange={e => changeSearchValue(e.target.value)} />
+                <select className='selectPokeType' onChange={onTypeFilterChange}>
+                    <option value='none' selected>none</option>
+                    {pokeTypes.map(type => <option value={type.name} key={type.name}>{type.name}</option>)}
                 </select>
             </div>
-            
-            <PokemonList/>
+
+            {/* <InfiniteScroll
+                dataLength={itemsPerPage} //This is important field to render the next data
+                next={fetchNextPokemons}
+                hasMore={hasMore}
+                loader={<Loader />}
+                endMessage={<EndMsg />}
+                > */}
+            {isLoading ? <Loader /> : <PokemonList />}
+            {/* </InfiniteScroll> */}
             {!isMobile && <Pagination>
                 <div>
-                    <ReactPaginate 
+                    <ReactPaginate
                         previousLabel={"Previous"}
                         nextLabel={"Next"}
                         onPageChange={changePage}
@@ -95,7 +131,7 @@ const App = () => {
                 <option value='200'>200</option>
                 <option value={pokemonCount}>All</option>
             </select>}
-    </MainWrapper>
+        </MainWrapper>
     )
 }
 
